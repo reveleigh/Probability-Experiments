b0ab10ff45123dc8e266e6c2eb3b05542ae40db7
Russell Eveleigh <russelleveleigh@gmail.com>
2022-10-15 22:55:04 +0100

    Moved set coin probablities to useEffect

--- src/components/Layout/Controls.js
+++ src/components/Layout/Controls.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Navigation from "./Navigation";
 import Title from "./Title";
 import CoinStats from "../CoinToss/CoinStats";
@@ -11,19 +11,25 @@ const Controls = (props) => {
     const [PHeads, setPHeads] = useState((0).toFixed(5));
     const [PTails, setPTails] = useState((0).toFixed(5));
 
+
     //function to flip coin
+    
     const flipCoin = () => {
         const coin = Math.floor(Math.random() * 2);
         if (coin === 0) {
-            setHeads(heads + 1);
+            setHeads(heads + 1);         
         } else {
             setTails(tails + 1);
-        }
+        }      
+    };
+
+    useEffect(() => {
+
         if (heads + tails > 0) {
             setPHeads((heads / (heads + tails)).toFixed(5));
             setPTails((tails / (heads + tails)).toFixed(5));
         }
-    };
+    }, [heads, tails]);
 
     return(
         <div className="md:col-span-1 md:flex flex-col md:justify-start bg-white">
