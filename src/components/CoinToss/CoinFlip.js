1615531705717d0ce8d0665ec640d9006df302ae
Russell Eveleigh <russelleveleigh@gmail.com>
2022-10-20 22:52:09 +0100

    user-select none added

--- src/components/CoinToss/CoinFlip.js
+++ src/components/CoinToss/CoinFlip.js
@@ -3,7 +3,7 @@ const CoinFlip = (props) => {
     return(
     <div className="flex justify-center p-4">
         <div onClick={props.flip} className="py-2 px-8 text-center text-2xl border-4 cursor-pointer border-gray-600 bg-gray-100 rounded-full w-24 h-24 flex justify-center items-center">
-          <div className="flex">FLIP</div>
+          <div className="flex select-none">FLIP</div>
         </div>
       </div>
     );
--- src/components/Layout/Results.js
+++ src/components/Layout/Results.js
@@ -1,17 +1,17 @@
 const Results = (props) => {
   return (
     <main className="px-16 py-6 bg-gray-100 md:col-span-2 justify-between h-screen">
-      <h1 className="uppercase font-bold text-2xl border-b text-center">
+      <h1 className="select-none uppercase font-bold text-2xl border-b text-center">
         Results
       </h1>
 
-      <div className="py-4 flex gap-2 flex-wrap justify-center">
+      <div className="py-4 flex gap-2 flex-wrap select-none">
         {props.coins.map((coins, index) => (
           <div
             key={index}
             className={coins===0?"h-24 bg-red-100 border-4 border-red-600 rounded-full w-24 flex items-center justify-center text-6xl":"h-24 bg-amber-100 border-4 border-amber-600 rounded-full w-24 flex items-center justify-center text-6xl"}
           >
-            <div className={coins===0?"flex text-red-300":"flex text-amber-300"}>{coins === 0 ? "H" : "T"}</div>
+            <div className={coins===0?"flex select-none text-red-300":"flex text-amber-300"}>{coins === 0 ? "H" : "T"}</div>
           </div>
         ))}
       </div>
