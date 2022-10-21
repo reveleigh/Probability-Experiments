8a36211f684b37298a80ba7bad0ba287148122fa
Russell Eveleigh <russelleveleigh@gmail.com>
2022-10-21 21:53:48 +0100

    Reduced size of coins

--- src/components/Layout/Results.js
+++ src/components/Layout/Results.js
@@ -1,4 +1,6 @@
 const Results = (props) => {
+  const h = "h-16 bg-red-100 border-2 border-red-600 rounded-full w-16 flex items-center justify-center text-4xl";
+  const t = "h-16 bg-amber-100 border-2 border-amber-600 rounded-full w-16 flex items-center justify-center text-4xl";
   return (
     <main className="px-16 py-6 bg-gray-100 md:col-span-2 justify-between h-screen overflow-scroll">
       <h1 className="select-none uppercase font-bold text-2xl border-b text-center">
@@ -9,7 +11,7 @@ const Results = (props) => {
         {props.coins.map((coins, index) => (
           <div
             key={index}
-            className={coins===0?"h-24 bg-red-100 border-4 border-red-600 rounded-full w-24 flex items-center justify-center text-6xl":"h-24 bg-amber-100 border-4 border-amber-600 rounded-full w-24 flex items-center justify-center text-6xl"}
+            className={coins===0?h:t}
           >
             <div className={coins===0?"flex select-none text-red-300":"flex text-amber-300"}>{coins === 0 ? "H" : "T"}</div>
           </div>
